b372ab666829ce3bf67e9a892bad785fe765b90f
Wenjie Shi <wenjieshi1@gmail.com>
2019-10-20 11:02:43 -0700

    Added Emacs resources

---
+++ resources/code_snippet.js
@@ -0,0 +1,12 @@
+// Useful comment
+function func(param) {
+  var text = 'str'
+  for (var i = 0; i < param.length; i++) {
+    text += i;
+  }
+
+  return {
+    "test" : test,
+    "boolean" : false
+  }
+}
